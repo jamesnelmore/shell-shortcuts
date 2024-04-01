31d4df1a2078f7c23f31b139951527009a584611
James Elmore <email@jameselmore.org>
2024-04-01 13:40:49 -0400

    Worked on buffer parsing

--- src/aliases.rs
+++ src/aliases.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use regex::{Regex, RegexBuilder};
 
 #[derive(Debug, PartialEq)]
 pub struct Alias {
@@ -18,16 +18,18 @@ impl Alias {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct AliasList {
     pub aliases: Vec<Alias>,
 }
 
 impl AliasList {
+    #[allow(unused)]
     pub fn new() -> AliasList {
         AliasList { aliases: vec![] }
     }
 
+    #[allow(unused)]
     pub fn add_alias(&mut self, alias: Alias) -> Option<()> {
         if self.aliases.iter().any(|a| a.shortcut == alias.shortcut) {
             return None;
@@ -36,7 +38,7 @@ impl AliasList {
 
         Some(())
     }
-
+    #[allow(unused)]
     pub fn to_buffer(&self) -> String {
         self.aliases
             .iter()
@@ -44,33 +46,45 @@ impl AliasList {
             .collect::<Vec<_>>()
             .join("\n")
     }
-}
 
-fn aliases_from_buf(buf: &str) -> AliasList {
-    // TODO Document REGEX
-    const REGEX_STRING: &str = r#"^(?:alias )(?<shortcut>\S+)(?: ?= ?")(?<command>\S+)(?:")$"#;
+    #[allow(unused)]
+    const REGEX_STRING: &'static str = concat!(
+        r"^(:alias )",             // Lookbehind matching "alias " at start of line
+        r"(?<shortcut>.+)",        // Matches text after "alias " under "="
+        r#"(?: ?= ?")"#,           // Matches but does not capture "="
+        r#"(?<command>.+)(?:")$"#, // Matches all text between quotes and end of line
+    );
 
-    #[allow(clippy::unwrap_used)]
-    let regex: Regex = Regex::new(REGEX_STRING).unwrap();
+    #[allow(unused)]
+    fn get_regex() -> Regex {
+        #[allow(clippy::unwrap_used)]
+        RegexBuilder::new(AliasList::REGEX_STRING)
+            .multi_line(true)
+            .build()
+            .unwrap()
+    }
 
-    let aliases = regex
-        .captures_iter(buf)
-        .map(|capture| {
-            Alias::new(
-                capture["shortcut"].to_string(),
-                capture["command"].to_string(),
-            )
-        })
-        .collect::<Vec<Alias>>();
+    #[allow(unused)]
+    fn aliases_from_buf(buf: &str) -> AliasList {
+        let aliases = Self::get_regex()
+            .captures_iter(buf)
+            .map(|capture| {
+                let shortcut = &capture["shortcut"].to_string();
+                let command = &capture["command"].to_string();
+                println!("Captured: {shortcut} | {command}");
+                Alias::new(shortcut, command)
+            })
+            .collect::<Vec<Alias>>();
 
-    AliasList { aliases }
+        AliasList { aliases }
+    }
 }
 
 #[cfg(test)]
 mod test_alias_list {
     use super::*;
-    use indoc;
-    use rstest::{fixture};
+    use indoc::indoc;
+    use rstest::{fixture, rstest};
 
     #[fixture]
     fn sample_buf() -> &'static str {
@@ -81,12 +95,20 @@ mod test_alias_list {
         "#}
     }
 
-    #[test]
-    fn aliases_from_buf_happy_path(sample_buf: &str) {
-        let aliases = aliases_from_buf(sample_buf);
-        let test_buffer = aliases.to_buffer();
+    #[fixture]
+    fn sample_buf_aliases() -> AliasList {
+        AliasList {
+            aliases: vec![
+                Alias::new("cmd", "Shortcut"),
+                Alias::new("thing", "Do this"),
+                Alias::new("gs", "git status"),
+            ],
+        }
+    }
 
-        assert_eq!(sample_buf, test_buffer);
+    #[rstest]
+    fn aliases_from_buf_happy_path(sample_buf: &str, sample_buf_aliases: AliasList) {
+        assert_eq!(AliasList::aliases_from_buf(sample_buf), sample_buf_aliases);
     }
 }
 
