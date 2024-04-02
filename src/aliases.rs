58a08d707a00bc6ff4615cdafd008d2c0e131611
James Elmore <email@jameselmore.org>
2024-04-01 23:37:43 -0400

    Refactored and added tests

--- src/aliases.rs
+++ src/aliases.rs
@@ -38,6 +38,7 @@ impl AliasList {
 
         Some(())
     }
+
     #[allow(unused)]
     pub fn to_buffer(&self) -> String {
         self.aliases
@@ -84,10 +85,12 @@ impl AliasList {
 mod test_alias_list {
     use super::*;
     use indoc::indoc;
+    use regex::Captures;
     use rstest::{fixture, rstest};
 
     #[fixture]
     fn sample_buf() -> &'static str {
+        // TODO parameterize with more cases
         indoc! {r#"
           alias scut="cmd"
           alias thing="Do this"
@@ -96,7 +99,8 @@ mod test_alias_list {
     }
 
     #[fixture]
-    fn sample_buf_aliases() -> AliasList {
+    fn sample_aliases() -> AliasList {
+        // TODO parameterize with more cases
         AliasList {
             aliases: vec![
                 Alias::new("scut", "cmd"),
@@ -106,26 +110,59 @@ mod test_alias_list {
         }
     }
 
+    fn match_text(haystack: &str) -> Option<Captures> {
+        // TODO parameterize with more cases
+        AliasList::get_regex().captures(haystack)
+    }
+
     // Test Regular Expression
 
     #[rstest]
     fn parse_regex_single_example() {
+        // TODO parameterize with more cases
         let haystack = r#"alias scut="Do this""#;
 
-        match AliasList::get_regex().captures(haystack) {
+        match match_text(haystack) {
             Some(capture) => {
                 assert_eq!(&capture["shortcut"], "scut");
                 assert_eq!(&capture["command"], "Do this");
             }
-            None => {
-                panic!("Expected a match")
-            }
+            None => panic!("Expected a match"),
         }
     }
 
     #[rstest]
-    fn aliases_from_buf_happy_path(sample_buf: &str, sample_buf_aliases: AliasList) {
-        assert_eq!(AliasList::aliases_from_buf(sample_buf), sample_buf_aliases);
+    fn parse_regex_spaces_around_equals() {
+        // TODO parameterize with more cases
+        let captures = match_text(r#"alias scut = "Do this"#);
+        assert!(Option::is_none(&captures)); // Returns none when there is no match
+    }
+
+    #[rstest]
+    fn aliases_from_buf_happy_path(sample_buf: &str, sample_aliases: AliasList) {
+        // TODO parameterize with more cases
+        assert_eq!(AliasList::aliases_from_buf(sample_buf), sample_aliases);
+    }
+
+    #[rstest]
+    fn add_alias_happy_path() {
+        // TODO parameterize by empty and nonempty lists
+
+        let mut aliases = AliasList::new();
+        aliases.add_alias(Alias::new("gs", "git status")).unwrap();
+        assert_eq!(aliases.aliases, vec![Alias::new("gs", "git status")]);
+    }
+
+    #[rstest]
+    fn add_alias_duplicate(mut sample_aliases: AliasList) {
+        // TODO parameterize with more cases
+        let alias = Alias::new("gs", "git status");
+        assert!(
+            sample_aliases.aliases.contains(&alias),
+            "Test conditions not met. Ensure `alias` is in `sample_buf_aliases`."
+        );
+
+        assert_eq!(sample_aliases.add_alias(alias), None);
     }
 }
 
@@ -135,6 +172,7 @@ mod test_alias {
 
     #[test]
     fn test_alias_equality() {
+        // TODO Parameterize with more cases
         let alias1 = Alias::new("Foo", "Bar");
         let alias2 = Alias::new("Foo", "Bar");
         assert_eq!(alias1, alias2);
@@ -142,6 +180,7 @@ mod test_alias {
 
     #[test]
     fn test_alias_inequality() {
+        // TODO parameterize with more cases
         assert_ne!(Alias::new("Foo", "Bar"), Alias::new("Baz", "Bor"));
     }
 }
