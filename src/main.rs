d6eb705a2201232281a08645a9905bc3c447e4d2
James Elmore <email@jameselmore.org>
2024-04-06 12:40:55 -0400

    Partially implemented add command

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,29 @@ mod alias;
 mod aliases;
 mod interface;
 
+use alias::Alias;
+use aliases::{AliasFile, AliasList};
 use clap::Parser;
 use interface::{Commands, Interface};
-use std::path::PathBuf;
 use std::error::Error;
-
-use aliases::{AliasList, AliasFile};
+use std::path::PathBuf;
 // Plan
 // Initialize AliasList
 // - Check environment variables for path to alias file
 // - Parse alias file for AliasList
 // Execute user command with given AliasList
 
-fn main() -> Result<(), Box<dyn Error>>{
-    let mut alias_file = AliasFile::new(alias_path());
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut alias_file = AliasFile::new(alias_path())?;
     let interface = Interface::parse();
     match &interface.command {
         Commands::Add { shortcut, command } => {
-            todo!("Add {shortcut}=\"{command}\"")
+            let alias: Alias = Alias::new(shortcut, command).or_else(|_| {
+                println!("Invalid input");
+                todo!("Graceful error handling for invalid aliases");
+            })?;
+            alias_file.aliases().add_alias(alias);
+            todo!("Save file")
         }
         Commands::Remove { .. } => {
             todo!("implement removing aliases")
@@ -31,7 +36,7 @@ fn main() -> Result<(), Box<dyn Error>>{
             todo!("implement replacement")
         }
         Commands::List => {
-            let display = alias_file?.aliases().to_string();
+            let display = alias_file.aliases().to_string();
             println!("{display}");
         }
     };
